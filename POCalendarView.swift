74837356a5140c94a1578dec4f039e4935869787
ponchan <ponchan@ponchan.local>
2016-12-11 19:32:28 +0900

    Made the wareki-dic concurrent

--- POCalendarView.swift
+++ POCalendarView.swift
@@ -20,10 +20,25 @@ class POCalendarView : NSView {
         get{
             return "\(year)/\(String(format: "%02d", month))/\(String(format: "%02d", day))"
         }
+        
+        set {
+            let date = newValue.components(separatedBy: "/")
+            if(date.count != 3) { return }
+            
+            year = Int(date[0])!
+            month = Int(date[1])!
+            day = Int(date[2])!
+            
+            initYears()
+            
+        }
     }
     var stringValue: String {
         get{
             return text
+        }
+        set {
+            text = newValue
         }
     }
     
@@ -35,7 +50,9 @@ class POCalendarView : NSView {
     private let _dateformatter = DateFormatter()
     private let _dateformatterja = DateFormatter()
 
-    private var _warekiDictionary = [String:String]()
+    private static var _warekiDictionary: [String:String]!
+    private static let _group = DispatchGroup() // 辞書作成が重いため
+
 
     @IBAction func onSelected(_ sender: NSPopUpButton) {
         
@@ -107,22 +124,26 @@ class POCalendarView : NSView {
     private func initYears() {
         
         _year.removeAllItems()
-        // soted keys
-        let keys = _warekiDictionary.keys.sorted { (a, b) -> Bool in
-            a > b
+
+        DispatchQueue.global(qos: .default).async {
+            // soted keys
+            let keys = POCalendarView._warekiDictionary.keys.sorted { (a, b) -> Bool in
+                a > b
+            }
+            
+            for k in keys {
+                let str = " \(k) 年（\(POCalendarView._warekiDictionary["\(k)"]!)）"
+                let item = NSMenuItem()
+                item.title = str
+                item.representedObject = Int(k)
+                self._year.menu?.addItem(item)
+                if(String(self.year) == k) {
+                    self._year.select(item)
+                }
+            }
+            
+            self.initMonth()
         }
-        
-        for k in keys {
-            let str = " \(k) 年（\(_warekiDictionary["\(k)"]!)）"
-            let item = NSMenuItem()
-            item.title = str
-            item.representedObject = Int(k)
-            _year.menu?.addItem(item)
-//            _year.addItem(withTitle: str)
-        }
-        
-        initMonth()
-        
 //        for c in _warekiDictionary {
 //            let str = "\(c.key)年 （\(c.value)）"
 //            _year.addItem(withTitle: str)
@@ -142,45 +163,52 @@ class POCalendarView : NSView {
     
     private func initWarekiDictionary() {
         
-        let now = Date()
-        let componentsNow = _dateformatter.calendar.dateComponents([.year, .month], from: now)
-        _dateformatterja.dateFormat = "Gy年"
-        
-        let currentYear = componentsNow.year!
-        var arr = [[String]]()
-        
-        for i in 0..<duration {
-            let y = currentYear - i
+        if(POCalendarView._warekiDictionary != nil) { return }
+
+        DispatchQueue.global(qos: .default).async(group: POCalendarView._group) {
+
+            POCalendarView._warekiDictionary = [String:String]()
             
-            var prevWareki = ""
-            var tempArr = [String]()
-            for m in 1...12 {
-                for d in 1...31 {
-                    if let date = _dateformatter.date(from: "\(y)/\(m)/\(d)") {
-                        let wareki = _dateformatterja.string(from:date)
-                        if( prevWareki != wareki) {
-                            tempArr.append(wareki)
+            let now = Date()
+            let componentsNow = self._dateformatter.calendar.dateComponents([.year, .month], from: now)
+            self._dateformatterja.dateFormat = "Gy年"
+            
+            let currentYear = componentsNow.year!
+            var arr = [[String]]()
+            
+            for i in 0..<self.duration {
+                let y = currentYear - i
+                
+                var prevWareki = ""
+                var tempArr = [String]()
+                for m in 1...12 {
+                    for d in 1...31 {
+                        if let date = self._dateformatter.date(from: "\(y)/\(m)/\(d)") {
+                            let wareki = self._dateformatterja.string(from:date)
+                            if( prevWareki != wareki) {
+                                tempArr.append(wareki)
+                            }
+                            prevWareki = wareki
                         }
-                        prevWareki = wareki
                     }
                 }
+                
+                arr.append(tempArr)
             }
             
-            arr.append(tempArr)
-        }
-        
-        // Creare Wareki dictionary
-        var y = currentYear
-        for a in arr {
-            var i = a.count - 1
-            var yearStr = a[i]
-            i -= 1
-            while(i > -1) {
-                yearStr += "、" + a[i]
+            // Creare Wareki dictionary
+            var y = currentYear
+            for a in arr {
+                var i = a.count - 1
+                var yearStr = a[i]
                 i -= 1
+                while(i > -1) {
+                    yearStr += "、" + a[i]
+                    i -= 1
+                }
+                POCalendarView._warekiDictionary["\(y)"] = yearStr
+                y -= 1
             }
-            _warekiDictionary["\(y)"] = yearStr
-            y -= 1
         }
     }
     
@@ -192,7 +220,7 @@ class POCalendarView : NSView {
         _dateformatterja.calendar = Calendar(identifier: Calendar.Identifier.japanese)
         
         // 年号辞書作成
-        initWarekiDictionary()
+        self.initWarekiDictionary()
         
         // 今日が初期値
         let now = _dateformatter.calendar.dateComponents([.year, .month, .day], from: Date())
@@ -201,12 +229,15 @@ class POCalendarView : NSView {
         day = now.day!
         
         // 年月一覧更新
-        initYears() //　すべて連続で初期化される
-
-        // 初期時にだけ、現在の日付を適用
-        _year.selectItem(at: 0)
-        _month.selectItem(at: 0)
-        _day.selectItem(at: 0)
+        
+        POCalendarView._group.notify(queue: DispatchQueue.global(qos: .default)) {
+            self.initYears() //　すべて連続で初期化される
+            
+            // 初期時にだけ、現在の日付を適用
+            self._year.selectItem(at: 0)
+            self._month.selectItem(at: 0)
+            self._day.selectItem(at: 0)
+        }
     }
     
     // shared init
